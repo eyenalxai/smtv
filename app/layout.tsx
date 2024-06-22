987d6ef9f90ec147994915dc01d1cf071821387b
Sax Dhiut <mail@iyxeyl.me>
2024-06-22 21:18:11 +0400

    feat: add viewport settings for responsive design

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,10 @@ export const metadata: Metadata = {
 }
 
 export const viewport: Viewport = {
+	width: "device-width",
+	initialScale: 1,
+	maximumScale: 1,
+	userScalable: false,
 	themeColor: [
 		{ media: "(prefers-color-scheme: light)", color: "0 0% 100%" },
 		{ media: "(prefers-color-scheme: dark)", color: "222.2 84% 4.9%" }
