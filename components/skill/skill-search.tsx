3c9d400bdc819840126d87208ded7b640ce03f31
Sax Dhiut <mail@iyxeyl.me>
2024-06-22 21:32:01 +0400

    feat: add clear button to skill search input

--- components/skill/skill-search.tsx
+++ components/skill/skill-search.tsx
@@ -1,16 +1,23 @@
 "use client"
 
 import { SkillFetcher } from "@/components/skill/skill-fetcher"
+import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { useSkillName } from "@/lib/hooks/skill-name-context"
 import { cn } from "@/lib/utils"
+import { X } from "lucide-react"
 
 export const SkillSearch = () => {
 	const { skillName, setSkillName } = useSkillName()
 
 	return (
 		<div className={cn("flex", "flex-col", "items-center", "justify-center", "gap-8", "w-full")}>
-			<Input placeholder={"Skill"} value={skillName} onChange={(e) => setSkillName(e.target.value)} />
+			<div className={cn("flex", "flex-row", "justify-center", "items-center", "gap-4")}>
+				<Input placeholder={"Skill"} value={skillName} onChange={(e) => setSkillName(e.target.value)} />
+				<Button variant="outline" onClick={() => setSkillName("")}>
+					<X className={"size-4"} />
+				</Button>
+			</div>
 			{skillName.length >= 2 && <SkillFetcher skillName={skillName} />}
 		</div>
 	)
